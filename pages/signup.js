6b2db056c66a8b75fd4b717503a1621e001e0afb
livinoid98 <ccy980@naver.com>
2021-08-22 23:46:45 +0900

    add term check message

--- pages/signup.js
+++ pages/signup.js
@@ -8,8 +8,17 @@ const Signup = () => {
     const [password, setPassword] = useState('');
     const [passwordCheck, setPasswordCheck] = useState('');
     const [term, setTerm] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [termError, setTermError] = useState(false);
+
     const onSubmit = (e) => {
         e.preventDefault();
+        if(password !== passwordCheck) {
+            return setPasswordError(true);
+        }
+        if(!term) {
+            return setTermError(true);
+        }
         console.log({
             id,
             nick,
@@ -18,19 +27,21 @@ const Signup = () => {
             term
         });
     };
-    const onChangeId = () => {
+    const onChangeId = (e) => {
         setId(e.target.value);
     };
-    const onChangeNick = () => {
+    const onChangeNick = (e) => {
         setNick(e.target.value);
     };
-    const onChangePassword = () => {
+    const onChangePassword = (e) => {
         setPassword(e.target.value);
     };
-    const onChangePasswordCheck = () => {
+    const onChangePasswordCheck = (e) => {
+        setPasswordError(e.target.value !== password);
         setPasswordCheck(e.target.value);
     };
-    const onChangeTerm = () => {
+    const onChangeTerm = (e) => {
+        setTermError(true);
         setTerm(e.target.checked);
     };
     return (
@@ -39,27 +50,29 @@ const Signup = () => {
                 <div>
                     <label htmlFor="user-id">아이디</label>
                     <br/>
-                    <Input name="user-id" value={id} required onChange={onChangeId}/>
+                    <Input name="user-id" required onChange={onChangeId}/>
                 </div>
                 <div>
                     <label htmlFor="user-nick">닉네임</label>
                     <br/>
-                    <Input name="user-nick" value={nick} required onChange={onChangeNick}/>
+                    <Input name="user-nick" required onChange={onChangeNick}/>
                 </div>
                 <div>
                     <label htmlFor="user-password">패스워드</label>
                     <br/>
-                    <Input name="user-password" value={password} type="password" required onChange={onChangePassword}/>
+                    <Input name="user-password" type="password" required onChange={onChangePassword}/>
                 </div>
                 <div>
                     <label htmlFor="user-pass-check">패스워드체크</label>
                     <br/>
-                    <Input name="user-pass-check" value={passwordCheck} type="password" required onChange={onChangePasswordCheck}/>
+                    <Input name="user-pass-check" type="password" required onChange={onChangePasswordCheck}/>
+                    {passwordError && <div style={{color: 'red'}}>비밀번호가 일치하지 않습니다.</div>}
                 </div>
                 <div>
-                    <Checkbox name="user-term" value={term} onChange={onChangeTerm}>위 약관에 동의합니다.</Checkbox>
+                    <Checkbox name="user-term" onChange={onChangeTerm}>위 약관에 동의합니다.</Checkbox>
+                    {!termError && <div style={{color: 'red'}}>약관에 동의하셔야 합니다.</div>}
                 </div>
-                <div>
+                <div style={{marginTop:10}}>
                     <Button type="primary">가입하기</Button>
                 </div>
             </Form>
